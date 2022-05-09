6b833a54beee5afd4005af789be174c5a1347743
vidsdn <vidsdn@gmail.com>
2022-05-09 11:53:37 -0500

    RestAPI Crud using spring boot and postgresql

---
+++ src/main/java/com/example/employeeSpringBoot/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.example.employeeSpringBoot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.employeeSpringBoot.entity.Employee;
+
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
